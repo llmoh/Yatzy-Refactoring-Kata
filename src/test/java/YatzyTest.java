e6aced2b00f81ca8840d12c59267c59278d7b16c
Mohamed <mohamed.llaidani@gmail.com>
2023-07-07 14:08:19 +0100

    Change unit tests to become parameterized tests

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,170 +1,142 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
+@DisplayName(value = "Test Yatzy Game")
+@TestMethodOrder(OrderAnnotation.class)
 public class YatzyTest {
 
-    @Test
-    public void chanceCategoryTest() {
-    	
-    	int[] expectedScores = { 15, 16, 20 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(2,3,4,5,1).chance());
-        assertEquals(expectedScores[1], new Yatzy(3,3,4,5,1).chance());
-        assertEquals(expectedScores[2], new Yatzy(6,5,4,3,2).chance());
+    @DisplayName("Test Chance Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/chance-data.csv", numLinesToSkip = 1)
+    @Order(10)
+    public void chanceCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).chance());
     }
 
-    @Test 
-    public void yatzyCategoryTest() {
-    	
-    	int[] expectedScores = { 0, 50 };
-      
-    	assertEquals(expectedScores[1], new Yatzy(4,4,4,4,4).yatzy());
-        assertEquals(expectedScores[1], new Yatzy(6,6,6,6,6).yatzy());
-        assertEquals(expectedScores[0], new Yatzy(6,6,6,6,3).yatzy());
+    @DisplayName("Test Yatzy Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/yatzy-data.csv", numLinesToSkip = 1) 
+    @Order(20)
+    public void yatzyCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).yatzy());
     }
 
-    @Test 
-    public void onesCategoryTest() {
-    	
-    	int[] expectedScores = { 0, 1, 2, 4 };
-    	
-    	assertEquals(expectedScores[0], new Yatzy(6,2,2,4,5).ones());
-        assertEquals(expectedScores[1], new Yatzy(1,2,3,4,5).ones());
-        assertEquals(expectedScores[2], new Yatzy(1,2,1,4,5).ones());
-        assertEquals(expectedScores[3], new Yatzy(1,2,1,1,1).ones());
+    @DisplayName("Test Ones Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/ones-data.csv", numLinesToSkip = 1)  
+    @Order(30)
+    public void onesCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).ones());
     }
 
-    @Test
-    public void twosCategoryTest() {
-    	
-    	int[] expectedScores = { 4, 10, 2 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(1,2,3,2,6).twos());
-        assertEquals(expectedScores[1], new Yatzy(2,2,2,2,2).twos());
-        assertEquals(expectedScores[2], new Yatzy(2,1,1,4,4).twos());
-        
+    @DisplayName("Test Twos Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/twos-data.csv", numLinesToSkip = 1) 
+    @Order(40)
+    public void twosCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).twos());
     }
 
-    @Test
-    public void threesCategoryTest() {
-    	
-    	int[] expectedScores = { 6, 12, 3 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(1,2,3,2,3).threes());
-        assertEquals(expectedScores[1], new Yatzy(2,3,3,3,3).threes());
-        assertEquals(expectedScores[2], new Yatzy(1,6,2,3,4).threes());
+    @DisplayName("Test Threes Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/threes-data.csv", numLinesToSkip = 1) 
+    @Order(50)
+    public void threesCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).threes());
     }
 
-    @Test
-    public void foursCategoryTest() 
-    {
-    	int[] expectedScores = { 4, 8, 12 };
-    	
-    	assertEquals(expectedScores[0], new Yatzy(4,5,5,5,5).fours());
-        assertEquals(expectedScores[1], new Yatzy(4,4,5,5,5).fours());
-        assertEquals(expectedScores[2], new Yatzy(4,4,4,5,5).fours());
+    @DisplayName("Test Fours Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/fours-data.csv", numLinesToSkip = 1)
+    @Order(60)
+    public void foursCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5){
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).fours());
     }
 
-    @Test
-    public void fivesCategoryTest() {
-    	
-    	int[] expectedScores = { 10, 15, 20 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(4,4,4,5,5).fives());
-        assertEquals(expectedScores[1], new Yatzy(4,4,5,5,5).fives());
-        assertEquals(expectedScores[2], new Yatzy(4,5,5,5,5).fives());
+    @DisplayName("Test Fives Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/fives-data.csv", numLinesToSkip = 1) 
+    @Order(70)
+    public void fivesCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).fives());
     }
 
-    @Test
-    public void sixesCategoryTest() {
-    	
-    	int[] expectedScores = { 0, 6, 18 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(expectedScores[1], new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(expectedScores[2], new Yatzy(6,5,6,6,5).sixes());
+    @DisplayName("Test Sixes Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/sixes-data.csv", numLinesToSkip = 1) 
+    @Order(80)
+    public void sixesCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).sixes());
     }
 
-    @Test
-    public void onePairCategoryTest() {
-    	
-    	int[] expectedScores = { 6, 10, 12 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(3,4,3,5,6).scorePair());
-        assertEquals(expectedScores[1], new Yatzy(5,3,3,3,5).scorePair());
-        assertEquals(expectedScores[2], new Yatzy(5,3,6,6,5).scorePair());
+    @DisplayName("Test One Pair Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/onepair-data.csv", numLinesToSkip = 1)
+    @Order(90)
+    public void onePairCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).scorePair());
     }
 
-    @Test
-    public void twoPairCategoryTest() {
-    	
-    	int[] expectedScores = { 16, 16, 0 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(3,3,5,4,5).twoPair());
-        assertEquals(expectedScores[1], new Yatzy(3,3,5,5,5).twoPair());
-        assertEquals(expectedScores[2], new Yatzy(3,1,2,6,5).twoPair());
+    @DisplayName("Test Two Pair Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/twopair-data.csv", numLinesToSkip = 1) 
+    @Order(100)
+    public void twoPairCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).twoPair());
     }
 
-    @Test
-    public void threeOfAkindCategoryTest() 
-    {
-    	int[] expectedScores = { 9, 15, 9 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(3,3,3,4,5).threeOfAKind());
-        assertEquals(expectedScores[1], new Yatzy(5,3,5,4,5).threeOfAKind());
-        assertEquals(expectedScores[2], new Yatzy(3,3,3,3,5).threeOfAKind());
+    @DisplayName("Test Three Of a Kind Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/threekind-data.csv", numLinesToSkip = 1) 
+    @Order(110)
+    public void threeOfAkindCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).threeOfAKind());
     }
 
-    @Test
-    public void fourOfAKindCategoryTest() {
-    	
-    	int[] expectedScores = { 12, 20, 20 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(3,3,3,3,5).fourOfAKind());
-        assertEquals(expectedScores[1], new Yatzy(5,5,5,4,5).fourOfAKind());
-        assertEquals(expectedScores[2], new Yatzy(5,5,5,5,5).fourOfAKind());
+    @DisplayName("Test Four Of a Kind Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/fourkind-data.csv", numLinesToSkip = 1) 
+    @Order(120)
+    public void fourOfAKindCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).fourOfAKind());
     }
 
-    @Test
-    public void smallStraightCategoryTest() {
-    	
-    	int[] expectedScores = { 15, 15, 0 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(1,2,3,4,5).smallStraight());
-        assertEquals(expectedScores[1], new Yatzy(2,3,4,5,1).smallStraight());
-        assertEquals(expectedScores[2], new Yatzy(1,2,2,4,5).smallStraight());
+    @DisplayName("Test Small Straight Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/sm-straight-data.csv", numLinesToSkip = 1) 
+    @Order(130)
+    public void smallStraightCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).smallStraight());
     }
 
-    @Test
-    public void largeStraightCategoryTest() {
-    	
-    	int[] expectedScores = { 20, 20, 0 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(6,2,3,4,5).largeStraight());
-        assertEquals(expectedScores[1], new Yatzy(2,3,4,5,6).largeStraight());
-        assertEquals(expectedScores[2], new Yatzy(1,2,2,4,5).largeStraight());
+    @DisplayName("Test Large Straight Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/lg-straight-data.csv", numLinesToSkip = 1) 
+    @Order(140)
+    public void largeStraightCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).largeStraight());
     }
 
-    @Test
-    public void fullHouseCategoryTest() {
-    	
-    	int[] expectedScores = { 18, 17, 0 };
-    	
-        assertEquals(expectedScores[0], new Yatzy(6,2,2,2,6).fullHouse());
-        assertEquals(expectedScores[1], new Yatzy(3,4,3,4,3).fullHouse());
-        assertEquals(expectedScores[2], new Yatzy(2,3,4,5,6).fullHouse());
-     }
+    @DisplayName("Test Full House Category")
+    @ParameterizedTest(name = "Test N°{index} => ExpectedScore= {0}, dices=[{1},{2},{3},{4},{5}]")
+    @CsvFileSource(resources = "/fullhouse-data.csv", numLinesToSkip = 1) 
+    @Order(150)
+    public void fullHouseCategoryTest(int expectedScore, int d1, int d2, int d3, int d4, int d5) {
+    	assertEquals(expectedScore, new Yatzy(d1,d2,d3,d4,d5).fullHouse());
+    }
     
-    @Test
-    public void invalideDiceScoreTest() {
-    		
-    	assertThrows(IllegalArgumentException.class, ()->{new Yatzy(8,2,2,2,6);});
-    	assertThrows(IllegalArgumentException.class, ()->{new Yatzy(-1,0,2,2,7);});
-     }
+    @DisplayName("Test Invalid Dices Scores")
+    @ParameterizedTest(name = "Test N°{index} => dices=[{0},{1},{2},{3},{4}]")
+    @CsvFileSource(resources = "/invalid-data.csv", numLinesToSkip = 1) 
+    @Order(160)
+    public void invalidDiceScoreTest(int d1, int d2, int d3, int d4, int d5) {
+    	assertThrows(IllegalArgumentException.class, ()->{new Yatzy(d1,d2,d3,d4,d5);});
+    }
 }
